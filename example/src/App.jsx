89f541f4e7d040670245608379d0ed4f7969d305
Sakii <13037299+SakiiCode@users.noreply.github.com>
2024-12-07 16:20:21 +0100

    fix errors in example

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import {
+  BrowserRouter as Router, Switch, Route, Link,
+} from 'react-router-dom'
 
-import Download from './components/Download'
-import Image from './components/Image'
+import Download from './components/Download.jsx'
+import Image from './components/Image.jsx'
 
-export default () => {
-  return (
-    <Router>
-      <h1>Examples</h1>
-      <ul>
-        <li>
-          <Link to='/download'> Download </Link> -{' '}
-          <code>/components/Download.js</code>
-        </li>{' '}
-        <li>
-          <Link to='/image'> Image </Link> - <code>/components/Image.js</code>
-        </li>
-      </ul>
+export default () => (
+  <Router>
+    <h1>Examples</h1>
+    <ul>
+      <li>
+        <Link to='/download'> Download </Link> -{' '}
+        <code>/components/Download.js</code>
+      </li>{' '}
+      <li>
+        <Link to='/image'> Image </Link> - <code>/components/Image.js</code>
+      </li>
+    </ul>
 
-      <hr />
+    <hr />
 
-      <Switch>
-        <Route path='/download' component={Download} />
-        <Route path='/image' component={Image} />
-      </Switch>
-    </Router>
-  )
-}
+    <Switch>
+      <Route path='/download' component={Download} />
+      <Route path='/image' component={Image} />
+    </Switch>
+  </Router>
+)
